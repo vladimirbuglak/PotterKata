01a2c735192bc83a7990a78cdf23180e0e536eac
Uladzimir <u.buhlak@godeltech.com>
2017-05-25 12:47:41 +0300

    implement Calculate method

---
+++ PotterKata.BusinessLogic/DiscountInformation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace PotterKata.BusinessLogic
+{
+    public class DiscountInformation
+    {
+        public Dictionary<int, decimal> Discounts { get; set; }
+
+        public DiscountInformation()
+        {
+            Discounts = new Dictionary<int, decimal>
+            {
+                { 2, 5 },
+                { 3, 10 },
+                { 4, 15 },
+                { 5, 25 },
+                { 6, 30 },
+                { 7, 35 }
+            };
+        }
+
+        public decimal Get(int count)
+        {
+            if (count > 7)
+            {
+                return 35;
+            }
+            return count < 2 ? 0 : Discounts[count];
+        }
+    }
+}
--- PotterKata.BusinessLogic/Services/PriceCalculator.cs
+++ PotterKata.BusinessLogic/Services/PriceCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using PotterKata.BusinessLogic.Models;
 
 namespace PotterKata.BusinessLogic.Services
@@ -6,14 +7,22 @@ namespace PotterKata.BusinessLogic.Services
     public class PriceCalculator
     {
         private const decimal Price = 8;
+        private DiscountInformation DiscountInformation { get; }
 
         public PriceCalculator()
         {
+            DiscountInformation = new DiscountInformation();
         }
 
         public decimal Calculate(ICollection<Book> books)
         {
-            return 0;
+            var countOfBooksWithDiscount = books.GroupBy(x => x.Serie).Count();
+            var countOfBooksWithoutDiscount = books.Count - countOfBooksWithDiscount;
+
+            var discount = DiscountInformation.Get(countOfBooksWithDiscount);
+            var discountPrice = Price * (100 - discount) / 100;
+
+            return (countOfBooksWithDiscount * discountPrice) + (countOfBooksWithoutDiscount * Price);
         }
     }
 }
